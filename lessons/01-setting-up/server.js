9292bb58f1838319e9e5b6ea56b908c22d2bd857
何柯 <heke@hujiang.com>
2016-06-07 10:45:08 +0800

    absolute path resolve

--- lessons/01-setting-up/server.js
+++ lessons/01-setting-up/server.js
@@ -8,10 +8,10 @@ var compression = require('compression')
 var app = express()
 //must be first
 app.use(compression())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(path.resolve(__dirname), 'public')))
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(path.resolve(__dirname), 'public', 'index.html'))
 })
 
 var PORT = process.env.PORT || 8080
