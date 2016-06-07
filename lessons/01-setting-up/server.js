89d749387689f0d294de475715331a583fd2db88
何柯 <heke@hujiang.com>
2016-06-07 18:10:32 +0800

    服务端渲染

--- lessons/01-setting-up/server.js
+++ lessons/01-setting-up/server.js
@@ -1,20 +1,46 @@
 /**
  * Created by heoo442 on 2016/6/5.
  */
-var express = require('express')
-var path = require('path')
-var compression = require('compression')
+import express from 'express'
+import path from 'path'
+import compression from 'compression'
+import React from 'react';
+import {renderToString} from 'react-dom/server';
+import {match,RouterContext} from 'react-router';
+import  routes from './modules/routes';
 
 var app = express()
-//must be first
-app.use(compression())
-app.use(express.static(path.join(path.resolve(__dirname), 'public')))
+app.use(compression()); ////must be first
+app.use(express.static(path.join(path.resolve(__dirname), 'public'), {index: false}));
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(path.resolve(__dirname), 'public', 'index.html'))
-})
+app.get('*', (req, res) => {
+    match({routes: routes, location: req.url}, (err, redirect, props)=> {
+        if (err) {
+            res.status(500).send(err.message);
+        } else if (redirect) {
+            res.redirect(redirect.pathname + redirect.search);
+        } else if (props) {
+            const appHtml = renderToString(<RouterContext {...props}/>);
+            res.send(renderPage(appHtml));
+        } else {
+            res.status(404).send('Not Found');
+        }
+    });
+});
 
-var PORT = process.env.PORT || 8080
+function renderPage(appHtml) {
+    return `
+    <!doctype html public="storage">
+    <html>
+    <meta charset=utf-8/>
+    <title>My First React Router App</title>
+    <link rel="stylesheet" href=/index.css>
+    <div id=app>${appHtml}</div>
+    <script src="/bundle.js"></script>
+    `;
+}
+
+var PORT = process.env.PORT || 8088
 app.listen(PORT, function () {
     console.log('Production Express server running at localhost:' + PORT)
 })
